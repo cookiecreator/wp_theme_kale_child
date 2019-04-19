8f61afca49f0d6c108e7c38bf57dcb7488e469b7
p462042 <p462042@localdomain>
2019-04-19 22:53:41 +0200

    footer: remove credit

--- footer.php
+++ footer.php
@@ -5,11 +5,9 @@
 * @package kale
 */
 ?>
-
         <?php if(is_front_page() && !is_paged() ) { 
             get_template_part('parts/frontpage', 'large'); 
         } ?>
-
         <?php get_sidebar('footer'); ?>
         
         <!-- Footer -->
@@ -23,19 +21,12 @@
             <?php if($kale_footer_copyright) { ?>
             <div class="footer-copyright"><?php echo wp_kses_post($kale_footer_copyright); ?></div>
             <?php } ?>
-            
             <div class="footer-copyright">
-                <ul class="credit">
-                    <li><a href="https://www.lyrathemes.com/kale/"><?php esc_html_e('Kale', 'kale'); ?></a> <?php esc_html_e('by LyraThemes.com', 'kale'); ?></a>.</li>
-                </ul>
-            </div>
-            
+            </div>            
         </div>
-        <!-- /Footer -->
-        
+        <!-- /Footer -->     
     </div><!-- /Container -->
 </div><!-- /Main Wrapper -->
-
 <?php wp_footer(); ?>
 </body>
 </html>
